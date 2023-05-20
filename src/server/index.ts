29d11cdc31f9e56b8715e45296465cc67115c9a3
Judit Vives <jvivesramon@gmail.com>
2023-05-20 16:14:59 +0200

    Add Custom error and cors origins

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,19 @@
-import "../loadEnviroment.js";
+import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://202304-w7chwe-judit-vives-front.netlify.app",
+];
+
+const corsOptions: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.disable("x-powered-by");
 
