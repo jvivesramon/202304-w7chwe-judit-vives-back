26f00a29377b988fc03b8a2afa2d0a22a11ff13e
Judit Vives <jvivesramon@gmail.com>
2023-05-21 20:04:04 +0200

    Add loginUserController call json use case test

--- src/server/controllers/userController.test.ts
+++ src/server/controllers/userController.test.ts
@@ -12,40 +12,40 @@ import loginUserController from "./userControllers";
 
 describe("Given a loginUserController controller", () => {
   describe("When it receives a new user's data", () => {
+    const credentials: UserCredentials = {
+      username: "Jud",
+      password: "Jud",
+    };
+
+    const req: Pick<Request, "body"> = {
+      body: credentials,
+    };
+
+    const res: Pick<Response, "status" | "json"> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const next = jest.fn();
+
+    const expectedStatus = 200;
+
+    const userMock: UserData = {
+      _id: new Types.ObjectId().toString(),
+      username: "Jud",
+      password: "Jud",
+    };
+
+    const token = "someToken";
+
+    User.findOne = jest.fn().mockReturnValue({
+      exec: jest.fn().mockResolvedValue(userMock),
+    });
+
+    bcrypt.compare = jest.fn().mockResolvedValue(true);
+    jwt.sign = jest.fn().mockReturnValue(token);
+
     test("Then it should call the response's method method status with 200", async () => {
-      const credentials: UserCredentials = {
-        username: "Jud",
-        password: "Jud",
-      };
-
-      const req: Pick<Request, "body"> = {
-        body: credentials,
-      };
-
-      const res: Pick<Response, "status" | "json"> = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-
-      const next = jest.fn();
-
-      const expectedStatus = 200;
-
-      const userMock: UserData = {
-        _id: new Types.ObjectId().toString(),
-        username: "Jud",
-        password: "Jud",
-      };
-
-      const token = "someToken";
-
-      User.findOne = jest.fn().mockReturnValue({
-        exec: jest.fn().mockResolvedValue(userMock),
-      });
-
-      bcrypt.compare = jest.fn().mockResolvedValue(true);
-      jwt.sign = jest.fn().mockReturnValue(token);
-
       await loginUserController(
         req as UserCredentialsRequest,
         res as Response,
@@ -53,6 +53,16 @@ describe("Given a loginUserController controller", () => {
       );
 
       expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+
+    test("Then it should call the json method with an object with the token", async () => {
+      await loginUserController(
+        req as UserCredentialsRequest,
+        res as Response,
+        next
+      );
+
+      expect(res.json).toHaveBeenCalledWith({ token });
     });
   });
 });
